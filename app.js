386177d3eae5c0f99466f554b6d3c5e816fa9ce8
nextdev42 <jonathan.mrutu@protonmail.com>
2025-07-28 10:31:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import session from 'express-session';
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ extended: true }));
 
